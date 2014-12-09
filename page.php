8fbca3497934f8a59e267c65676c62caef1a7bdd
Ben Beekman <ben@benbeekman.com>
2014-12-09 13:25:20 -0800

    page template structural updates

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
 
 	<div id="inner-content" class="wrap clearfix">
 
-		<div id="main" class="twelvecol first clearfix" role="main">
+		<div id="main" class="m-all t-all d-all first clearfix" role="main">
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
