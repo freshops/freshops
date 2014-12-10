642ee55df97cdae77a1129fbff553bfff51beb1f
Micky Hulse <mickyhulse@gmail.com>
2014-12-09 20:50:39 -0800

    Added subhead/deck styles.

--- page.php
+++ page.php
@@ -36,35 +36,11 @@
 						
 					</div> <!-- /#submenu -->
 					
-				<?php endif; ?>
-				
-				<?php # What's this(?): ?>
-				
-				<?php
-				/*
-				
-				<?php if (have_rows('tab_panels')): ?>
+				<?php elseif (has_deck()): ?>
 					
-					<?php while (have_rows('tab_panels')): ?>
-						
-						<?php
-							the_row();
-							$section_title = get_sub_field('section_title');
-							$section_slug = sanitize_title($section_title);
-						?>
-						
-						<div id="<?php echo $section_slug; ?>">
-							<?php the_sub_field('section_content'); ?>
-						</div>
-						
-						<div class="clear"></div>
-						
-					<?php endwhile; ?>
+					<h2 class="sh4"><?php echo get_deck(); ?></h2>
 					
 				<?php endif; ?>
-				
-				*/
-				?>
 				
 			</header>
 			
