905cb21ab578f57b12e59ed6cb494b0a636eefd9
Micky Hulse <mickyhulse@gmail.com>
2014-12-10 02:43:05 -0800

    Cleaned up partials.

---
+++ includes/partials/content-alpha-acid-percentages.php
@@ -0,0 +1,64 @@
+<?php # The content loop for the alpha value page. ?>
+
+<?php
+	
+	# WP_Query arguments:
+	$args = array(
+		'post_type' => 'wpsc-product',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'wpsc_product_category',
+				'field' => 'slug',
+				'terms' => 'hop'
+			)
+		)
+	);
+	
+	# The Query:
+	$query = new WP_Query($args);
+	
+?>
+
+<?php if ($query->have_posts()): ?>
+	
+	<table class="table-01">
+		
+		<thead>
+			<tr>
+				<th scope="col">Name</th>
+				<th scope="col">Acid Range (Alpha %)</th>
+				<th scope="col">Flavor Perception</th>
+				<th scope="col">Commercial Example</th>
+			</tr>
+		</thead>
+		
+		<?php while ($query->have_posts()): ?>
+			
+			<?php $query->the_post(); ?>
+			
+			<tr>
+				<td><?php the_title(); ?></td>
+				<td><?php if (get_field('alpha')) { echo get_field('alpha'); } ?>&#37;</td>
+				<td><?php if (get_field('flavor')) { echo get_field('flavor'); } ?></td>
+				<td><?php if (get_field('example')) { echo get_field('example'); } ?></td>
+			</tr>
+			
+		<?php endwhile; ?>
+		
+	</table>
+	
+<?php else: ?>
+	
+	<!-- If query is empty, put a "not found" message here. -->
+	
+<?php endif; ?>
+
+<?php
+	
+	# Restore original Post Data:
+	wp_reset_postdata();
+	
+	# Display content of current post:
+	the_content();
+	
+?>
---
+++ includes/partials/content-none.php
@@ -0,0 +1,17 @@
+<?php # Page not found partial. ?>
+
+<article id="post-not-found" class="hentry clearfix">
+	
+	<header class="article-header">
+		<h1><?php _e('Oops, Post Not Found!', 'freshopstheme'); ?></h1>
+	</header>
+	
+	<section class="entry-content">
+		<p><?php _e('Uh Oh. Something is missing. Try double checking things.', 'freshopstheme'); ?></p>
+	</section>
+	
+	<footer class="article-footer">
+		<p><?php _e('This is the error message in the page.php template.', 'freshopstheme'); ?></p>
+	</footer>
+	
+</article>
