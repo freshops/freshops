61354d9ac00fac69c3de2e76f5531f4bbf154695
Ben Beekman <ben@benbeekman.com>
2014-12-10 00:26:15 -0800

    more partials

--- includes/partials/content-hop_alpha_values.php
+++ includes/partials/content-hop_alpha_values.php
@@ -1,101 +1,67 @@
-<?php
-/*
-Template Name: Hop Alpha Values
-*/
-?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-<?php get_header(); ?>
+	<header class="article-header">
 
-<div id="content">
+		<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
 
-	<div id="inner-content" class="wrap clearfix">
+	</header>
 
-		<div id="main" class="twelvecol first clearfix" role="main">
+	<section class="entry-content clearfix" itemprop="articleBody">
 
-
-			<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-				<div class="tabs">
-					<div class="alpha_values">
-						<header class="article-header">
-
-							<h1 class="page-title" itemprop="headline"><?php echo date('Y'); ?> <?php the_title(); ?></h1>
-							<h2>Certified Hop Analysis</h2>
-							<h3>USDA Hop Lab</h3>
-						</header>
-
-						<section class="entry-content clearfix" itemprop="articleBody">
-							<table class="table-01">
-
-								<thead>
-									<tr>
-										<th scope="col">Variety</th>
-										<th scope="col">Acid Range (Alpha %)</th>
-									</tr>
-								</thead>
-								<?php
+		<table class="table-01">
+			<thead>
+				<tr>
+					<th scope="col">Name</th>
+					<th scope="col">Acid Range (Alpha %)</th>
+					<th scope="col">Flavor Perception</th>
+					<th scope="col">Commercial Example</th>
+				</tr>
+			</thead>
+			<?php
 
 
 // WP_Query arguments
-								$args = array(
-								              'post_type' => 'wpsc-product',
-								              'tax_query' => array(
-								                                   array(
-								                                         'taxonomy' => 'wpsc_product_category',
-								                                         'field' => 'slug',
-								                                         'terms' => 'hop'
-								                                         )
-								                                   )
-								              );
+$args = array(
+'post_type' => 'wpsc-product',
+'tax_query' => array(
+array(
+'taxonomy' => 'wpsc_product_category',
+'field' => 'slug',
+'terms' => 'hop'
+)
+)
+);
 
 // The Query
-								$query = new WP_Query( $args );
+			$query = new WP_Query( $args );
 
 // The Loop
-								if ( $query->have_posts() ) {
-									while ( $query->have_posts() ) {
-										$query->the_post();
-										?>
-										<tr>
-											<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-											</td>
-											<td><?php if(get_field('alpha')) { 
-												echo get_field('alpha');} ?>&#37;</td>
-										</tr>
-										<?php
-									}
-								} else {
-	// no posts found
-								}
+			if ( $query->have_posts() ) {
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					?>
+					<tr>
+						<td><?php the_title();
+						 ?> </td>
+						<td><?php if(get_field('alpha')){ echo get_field('alpha');} ?>&#37;</td>
+						<td><?php if(get_field('flavor')){ echo get_field('flavor');} ?></td>
+						<td><?php if(get_field('example')){ echo get_field('example');} ?></td>
+					</tr>
+					<?php
+				}
+			} else {
+// no posts found
+			}
 
 // Restore original Post Data
-								wp_reset_postdata();
-								?>
-							</table>
-						</div>
-<?php the_content(); ?>
+			wp_reset_postdata();
 
-							</div>
-						</div>
-					</div>
-				</div>
+			the_content(); ?>
+		</table>
+	</section>
 
-			</div>
-		</div>
-</p>
-</p>
-</p>
-</p>
-</div>
-</div>
-</section>
-</div>
-</section>
-</div>
-</div>
+	<footer class="article-footer">
+	</footer>
+
+
 </article>
-</div>
-</div>
-</div>
-								<?php get_footer(); ?>
-
-
