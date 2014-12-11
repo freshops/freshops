3200fda2a2181d6c7a270154ba7b6a3ae09279bf
Ben Beekman <ben@benbeekman.com>
2014-12-11 13:19:16 -0800

    Single post template now uses include

--- single.php
+++ single.php
@@ -8,29 +8,7 @@
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-								<header class="article-header">
-
-									<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
-									<p class="byline vcard"><?php
-										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&amp;</span> filed under %4$s.', 'freshopstheme' ), get_the_time( 'Y-m-j' ), get_the_time( get_option('date_format')), freshops_get_the_author_posts_link(), get_the_category_list(', ') );
-									?></p>
-
-								</header>
-
-								<section class="entry-content clearfix" itemprop="articleBody">
-									<?php the_content(); ?>
-								</section>
-
-								<footer class="article-footer">
-									<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'freshopstheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-								</footer>
-
-								<?php comments_template(); ?>
-
-							</article>
+							<?php get_template_part('includes/partials/content-single', $name); ?>
 
 						<?php endwhile; ?>
 
