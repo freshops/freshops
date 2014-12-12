b3a3f43f69e9f9a2f3adf1daa26e85a29187a4cf
Ben Beekman <ben@benbeekman.com>
2014-12-11 16:22:19 -0800

    Fixes to single product hops template

--- includes/partials/content-single_hop_variety.php
+++ includes/partials/content-single_hop_variety.php
@@ -5,101 +5,101 @@ Single hop variety template for use in WP E-commerce single hop pages
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
-					<header class="article-header">
+	<header class="article-header">
 
-						<h1 class="single-title custom-post-type-title"><?php if(get_field('year')) {echo get_field('year');} ?> <?php the_title(); ?></h1>
-					</header>
+		<h1 class="single-title custom-post-type-title"><?php if(get_field('year')) {echo get_field('year');} ?> <?php the_title(); ?></h1>
+	</header>
 
-					<section class="entry-content clearfix">
-						<?php the_post_thumbnail( 'portrait-300' ); ?> </div>
-							<?php if(get_field('alpha')){ ?>
-							<dl><dt>Acid Range (Alpha &#37;):</dt>
-								<dd> <?php echo get_field('alpha'); ?></dd>
-							</dl>
-							<?php
-							}
+	<section class="entry-content clearfix">
+	<div class="alignleft"> <?php the_post_thumbnail( 'portrait-300' ); ?> </div>
+		
+		<?php if(get_field('alpha')) { ?>
+		<dl><dt>Acid Range (Alpha &#37;):</dt>
+			<dd> <?php echo get_field('alpha'); ?></dd>
+		</dl>
+		<?php } ?>
 
-							if(get_field('flavor')){ ?>
-								<dl><dt><?php the_title(); ?> Flavor Perception:</dt>
-									<dd> <?php echo get_field('flavor');?></dd>
-								</dl>
-								<?php
-							}
+		<?php if(get_field('flavor')){ ?>
+		<dl><dt><?php the_title(); ?> Flavor Perception:</dt>
+			<dd> <?php echo get_field('flavor');?></dd>
+		</dl>
+		<?php
+	}
 
-							if(get_field('example')){ ?>
-								<dl><dt>Commercial Examples: </dt>
-									<dd> <?php echo get_field('example');?></dd>
-								</dl>
-								<?php
-							}
-							if(get_field('beta')){ ?>
-								<dl><dt>Beta Range (&#37; of alpha acids)</dt>
-									<dd> <?php echo get_field('beta');?></dd>
-								</dl>
-								<?php
-							}
+	if(get_field('example')){ ?>
+	<dl><dt>Commercial Examples: </dt>
+		<dd> <?php echo get_field('example');?></dd>
+	</dl>
+	<?php
+}
+if(get_field('beta')){ ?>
+<dl><dt>Beta Range (&#37; of alpha acids)</dt>
+	<dd> <?php echo get_field('beta');?></dd>
+</dl>
+<?php
+}
 
-							if(get_field('cohumulone')){ ?>
-								<dl><dt>Cohumulone (&#37; of alpha acids)</dt>
-									<dd> <?php echo get_field('cohumulone');?></dd>
-								</dl>
-								<?php
-							}
-							if(get_field('total_oils')){ ?>
-								<dl><dt>Total Oils (Mls. per 100 grams dried hops)</dt>
-									<dd><?php echo get_field('cohumulone');?></dd></dl>
-								<?php
-							}
-							if(get_field('myrcene')){ ?>
-								<dl><dt>Caryophyllene (as &#37; of total oils)</dt>
-									<dd> <?php echo get_field('caryophyllene');?></dd>
-								</dl>
-								<?php
-							}
-							if(get_field('caryophyllene')){ ?>
-								<dl><dt>Caryophyllene (as &#37; of total oils)</dt>
-									<dd> <?php echo get_field('caryophyllene');?></dd>
-								</dl>
-								<?php
-							}
-							if(get_field('humulene')){ ?>
-								<dl><dt>Humulene (as &#37; of total oils)</dt>
-									<dd> <?php echo get_field('humulene');?></dd>
-								</dl>
-								<?php
-							}
-							if(get_field('farnesene')){ ?>
-								<dl><dt>Humulene (as &#37; of total oils)</dt>
-									<dd> <?php echo get_field('farnesene');?></dd>
-								</dl>
-								<?php
-							}
-							if(get_field('storage')){ ?>
-								<dl><dt>(as &#37; of alpha acids remaining after 6 months storage at 20&deg; C))</dt>
-									<dd> <?php echo get_field('storage');?></dd>
-								</dl>
-								<?php
-							}
-							if(get_field('possible_substitutions')){ ?>
-								<dl><dt>Possible Substitutions</dt>
-									<dd> <?php echo get_field('possible_substitutions');?></dd>
-								</dl>
-								<?php
-							}
-							if(get_field('usda_hops_info')){ ?>
-								<dl><dt>USDA Hops Information</dt>
-									<dd> <?php echo get_field('possible_substitutions');?></dd>
-								</dl>
-								<?php
-							}?>
+if(get_field('cohumulone')){ ?>
+<dl><dt>Cohumulone (&#37; of alpha acids)</dt>
+	<dd> <?php echo get_field('cohumulone');?></dd>
+</dl>
+<?php
+}
+if(get_field('total_oils')){ ?>
+<dl><dt>Total Oils (Mls. per 100 grams dried hops)</dt>
+	<dd><?php echo get_field('cohumulone');?></dd></dl>
+	<?php
+}
+if(get_field('myrcene')){ ?>
+<dl><dt>Caryophyllene (as &#37; of total oils)</dt>
+	<dd> <?php echo get_field('caryophyllene');?></dd>
+</dl>
+<?php
+}
+if(get_field('caryophyllene')){ ?>
+<dl><dt>Caryophyllene (as &#37; of total oils)</dt>
+	<dd> <?php echo get_field('caryophyllene');?></dd>
+</dl>
+<?php
+}
+if(get_field('humulene')){ ?>
+<dl><dt>Humulene (as &#37; of total oils)</dt>
+	<dd> <?php echo get_field('humulene');?></dd>
+</dl>
+<?php
+}
+if(get_field('farnesene')){ ?>
+<dl><dt>Humulene (as &#37; of total oils)</dt>
+	<dd> <?php echo get_field('farnesene');?></dd>
+</dl>
+<?php
+}
+if(get_field('storage')){ ?>
+<dl><dt>(as &#37; of alpha acids remaining after 6 months storage at 20&deg; C))</dt>
+	<dd> <?php echo get_field('storage');?></dd>
+</dl>
+<?php
+}
+if(get_field('possible_substitutions')){ ?>
+<dl><dt>Possible Substitutions</dt>
+	<dd> <?php echo get_field('possible_substitutions');?></dd>
+</dl>
+<?php
+}
+if(get_field('usda_hops_info')){ ?>
+<dl><dt>USDA Hops Information</dt>
+	<dd> <?php echo get_field('possible_substitutions');?></dd>
+</dl>
+<?php
+}?>
 
-						</div>
+</div>
 
-	</section>
+</section>
 
-	<footer class="article-footer">
-		<p class="tags"><?php echo get_the_term_list( get_the_ID(), 'custom_tag', '<span class="tags-title">' . __( 'Tagged with:', 'freshopstheme' ) . '</span> ', ', ' ) ?></p>
+<footer class="article-footer">
+	<p class="tags"><?php echo get_the_term_list( get_the_ID(), 'custom_tag', '<span class="tags-title">' . __( 'Tagged with:', 'freshopstheme' ) . '</span> ', ', ' ) ?></p>
 
-	</footer>
+</footer>
 
 </article>
