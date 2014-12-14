80dc19cabc2261d56127b2817b43abbec8d7106a
Micky Hulse <mickyhulse@gmail.com>
2014-12-13 22:44:08 -0800

    Page with sidebar
    Closes #16.

--- includes/page-sidebar.php
+++ includes/page-sidebar.php
@@ -1,3 +1,11 @@
+<?php
+	
+	/**
+	 * Template Name: Tabbed Content with Sidebar
+	 */
+	
+?>
+
 <?=get_header()?>
 
 <?php if (have_posts()): ?>
@@ -16,7 +24,21 @@
 			
 			<section id="content" itemprop="articleBody">
 				
-				<?=get_template_part('includes/partials/content', 'page')?>
+				<div class="fix">
+					
+					<div id="mainbar" class="m-all t-2of3 d-5of7">
+						
+						<?=get_template_part('includes/partials/content', 'page')?>
+						
+					</div> <!-- /#mainbar -->
+					
+					<div id="sidebar" class="sidebar m-all t-1of3 d-2of7 last-col" role="complementary">
+						
+						<?=get_sidebar()?>
+						
+					</div> <!-- /#sidebar -->
+					
+				</div> <!-- /.fix -->
 				
 			</section> <!-- /#content -->
 			
---
+++ includes/partials/content-page.php
@@ -0,0 +1,25 @@
+<?php
+	
+	global $post;
+	
+	$name = $post->post_name;
+	
+	# Add special partial template names here:
+	$special = array(
+		'alpha-acid-percentages',
+		'hops-rhizome-information',
+		'hop-variety-descriptions',
+		'testimonials',
+	);
+	
+?>
+
+<?php if ((is_single() || is_page()) && in_array($name, $special)): ?>
+	
+	<?=get_template_part('includes/partials/content', $name)?>
+	
+<?php else: ?>
+	
+	<?=get_template_part('includes/partials/content')?>
+	
+<?php endif; ?>
