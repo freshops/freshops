cd895626a9dc34469ccb2c23eb216455709c3fdc
Ben Beekman <ben@benbeekman.com>
2014-12-14 21:12:56 -0800

    hop field fixes

--- includes/cart/single_hop_variety.php
+++ includes/cart/single_hop_variety.php
@@ -3,8 +3,9 @@
 Single hop variety template for use in WP E-commerce single hop pages
 */
 ?>
-<h4>Hop Qualities</h4>
+
 <?php if(get_field('alpha-min')) { ?>
+<h4>Hop Qualities</h4>
 <dl>
 	<dt>Acid Range (Alpha &#37;):</dt>
 	<dd><?php echo get_field('alpha-min'); ?>&endash;<?php echo get_field('alpha-max'); ?>
