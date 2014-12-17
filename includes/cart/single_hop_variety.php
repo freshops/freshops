6e39b9990659969572bc82ee9af244bac9d1a247
Ben Beekman <ben@benbeekman.com>
2014-12-17 00:15:17 -0800

    2-column hop details

--- includes/cart/single_hop_variety.php
+++ includes/cart/single_hop_variety.php
@@ -5,142 +5,176 @@ Single hop variety template for use in WP E-commerce single hop pages
 ?>
 <h4>Hop Qualities</h4>
 
-<?php if(get_field('alpha-min')) : ?> <!-- Show alpha range if both min and max have values, otherwise show min value. -->
-	
-	<?php (float) $alphamin = get_field('alpha-min'); ?>
+<?php if(get_field('flavor')): ?>
 	
 	<dl>
-
-		<dt>Acid Range (Alpha &#37;):</dt>
-		<!-- End Display value -->
+	
+		<dt><?php the_title(); ?> Flavor Perception:</dt>
 		
-		<!-- if there's a max, there must be a min, so average the two to set alpha value for meter -->
-		<?php if(get_field('alpha-max')) : ?>
-			<?php (float) $alphamax = get_field('alpha-max'); ?>
-			<?php (float) $alphavalue = ( ( $alphamax+$alphamin)/2); ?> 
-			<?php $alphamin=$alphamin * 0.01; ?>
-			<?php $alphamax=$alphamax * 0.01; ?>
-			<? else: //otherwise alphamin is the alphavalue, so set it accordingly and reset min and max to null
-			$alphavalue = $alphamin;
-			unset ($alphamin); //reset alphamin
-			unset ($alphamax);
-			?>
-		<?php endif; ?>
+		<dd> <?php echo get_field('flavor');?></dd>
 		
-		<?php $alphapct = ( ( (float) $alphavalue) * 0.01); ?>
+	</dl>
+	
+<?php endif; ?>
 
-		<dd class='meter'>
-			<!-- meter begins here -->
-			<!-- set the low and high values based on values for low-alpha and high-alpha hops. -->
-			<meter
-			min="0.03"
-			max="0.14"
-			low="0.07"
-			high="0.01"
+<?php if(get_field('example')): ?>
+	
+	<dl>
+	
+		<dt>Commercial Examples: </dt>
+		
+		<dd><?php echo get_field('example');?></dd>
+		
+	</dl>
+	
+<?php endif; ?>
+
+<!-- Begin two-column percentages section -->
+<div class="percentage-columns">
+
+	<?php if(get_field('alpha-min')): ?> <!-- Show alpha range if both min and max have values, otherwise show min value. -->
+		
+		<?php (float) $alphamin = get_field('alpha-min'); ?>
+		
+		<dl>
+
+			<dt>Acid Range (Alpha &#37;)</dt>
+			<!-- End Display value -->
 			
-			value="<?=$alphapct?>"
+			<!-- if there's a max, there must be a min, so average the two to set alpha value for meter -->
+			<?php if(get_field('alpha-max')) : ?>
+				<?php (float) $alphamax = get_field('alpha-max'); ?>
+				<?php (float) $alphavalue = ( ( $alphamax+$alphamin)/2); ?> 
+				<?php $alphamin=$alphamin * 0.01; ?>
+				<?php $alphamax=$alphamax * 0.01; ?>
+				<? else: //otherwise alphamin is the alphavalue, so set it accordingly and reset min and max to null
+				$alphavalue = $alphamin;
+				unset ($alphamin); //reset alphamin
+				unset ($alphamax);
+				?>
+			<?php endif; ?>
 			
-			title="%"><?php  //print the min or only value, then if there's a max set, add an emdash and the max value.
+			<?php $alphapct = ( ( (float) $alphavalue) * 0.01); ?>
+
+			<dd class='meter'>
+				<!-- meter begins here -->
+				<!-- set the low and high values based on values for low-alpha and high-alpha hops. -->
+				<meter
+				min="0.03"
+				max="0.14"
+				low="0.07"
+				high="0.01"
+				
+				value="<?=$alphapct?>"
+				
+				title="%"><?php  //print the min or only value, then if there's a max set, add an emdash and the max value.
+					if (isset($alphamin))  : ?><?=$alphamin?><?php else : ?><?=$alphavalue;?><?php endif;
+					if (isset($alphamax)) : ?>–<?=$alphamax?><?php endif; ?>%</meter>
+			</dd><!-- meter ends here -->
+			
+					<!-- Display value starts here -->
+			<?php  //print the min or only value, then if there's a max set, add an emdash and the max value.
 				if (isset($alphamin))  : ?><?=$alphamin?><?php else : ?><?=$alphavalue;?><?php endif;
-				if (isset($alphamax)) : ?>–<?=$alphamax?><?php endif; ?>%</meter>
-		</dd><!-- meter ends here -->
+				if (isset($alphamax)) : ?>–<?=$alphamax?><?php endif; ?>%
+			
+		</dl>
+	<?php endif; ?>
+
+<!-- alpha section ends here -->
+
+
+<!-- beta section starts here -->
+
+	<?php if(get_field('beta-min')) : ?> <!-- Show beta range if both min and max have values, otherwise show min. -->
+		<dl>
+			<dt>Beta Range (% of alpha acids)</dt>
+
+			<dd>
+				<?php echo get_field('beta-min'); ?>
+				
+				<?php if (get_field('beta-max')) : 
+					//hyphen followed by max value (if one exists)
+					?>–<?php echo get_field('beta-max');
+				endif; ?>%
+			</dd>
+		</dl>
+	<?php endif; ?>
+
+	<!-- cohumulone starts here -->
+	<?php if(get_field('cohumulone')): ?>
 		
-				<!-- Display value starts here -->
-		<?php  //print the min or only value, then if there's a max set, add an emdash and the max value.
-			if (isset($alphamin))  : ?><?=$alphamin?><?php else : ?><?=$alphavalue;?><?php endif;
-			if (isset($alphamax)) : ?>–<?=$alphamax?><?php endif; ?>%
+		<dl>
 		
-	</dl>
-<?php endif; ?>
+			<dt>Cohumulone (% of alpha acids)</dt>
+			
+			<dd> <?php echo get_field('cohumulone');?>%</dd>
+			
+		</dl>
 
-
-<?php if(get_field('flavor')){ ?>
-<dl>
-	<dt><?php the_title(); ?> Flavor Perception:</dt>
-	<dd> <?php echo get_field('flavor');?></dd>
-</dl>
-<?php
-}
-
-if(get_field('example')){ ?>
-<dl>
-	<dt>Commercial Examples: </dt>
-	<dd> <?php echo get_field('example');?></dd>
-</dl>
-<?php
-}
-
-if(get_field('beta-min')) : ?> <!-- Show beta range if both min and max have values, otherwise show min value. -->
+	<?php endif; ?>
+	
+	<!-- cohumulone ends here -->
+	
+	<!-- total oils start here-->
+	
+	<?php if(get_field('total_oils')): ?>
+		<dl>
+			<dt>Total Oils (Mls. per 100 grams dried hops)</dt>
+			<dd><?php echo get_field('total_oils');?>%</dd></dl>
+	<?php endif; ?>
+	
+	<?php if(get_field('myrcene')): ?>
 	<dl>
-		<dt>Beta Range (&#37; of alpha acids)</dt>
+		<dt>Myrcene (as % of total oils)</dt>
+		<dd> <?php echo get_field('myrcene');?>%</dd>
+	</dl>
+	<?php endif; ?>
+	<?php if(get_field('caryophyllene')):?>
+		<dl>
+			<dt>Caryophyllene (as % of total oils)</dt>
+			<dd> <?php echo get_field('caryophyllene');?>%</dd>
+		</dl>
+	<?php
+	endif;
+	if(get_field('humulene')): ?>
+		<dl>
+			<dt>Humulene (as % of total oils)</dt>
+			<dd> <?php echo get_field('humulene');?>%</dd>
+		</dl>
+	<?php endif; ?>
 
-		<dd>
-			<?php echo get_field('beta-min'); 
-			?>–<?php if (get_field('beta-max')) :
-				echo get_field('beta-max');
-			endif; ?>%
-		</dd>
+	<?php if(get_field('farnesene')): ?>
+
+		<dl>
+
+			<dt>Farnesene (as % of total oils)</dt>
+			
+			<dd> <?php echo get_field('farnesene');?>%</dd>
+			
+		</dl>
+	<?php endif; ?>
+
+
+
+<?php if(get_field('storage')): ?>
+	<dl>
+		<dt>Storage (alpha acids remaining after 6 months storage at 20&deg; C))</dt>
+		<dd><?php echo get_field('storage');?></dd>
+	</dl>
+<?php endif;?>
+
+<?php if(get_field('possible_substitutions')): ?>
+	<dl>
+		<dt>Possible Substitutions</dt>
+		<dd> <?php echo get_field('possible_substitutions');?></dd>
+	</dl>
+<?php endif;?>
+
+</div><!-- end two-column percentage section -->
+
+<?php if(get_field('usda_hops_info')): ?>
+	<dl>
+		<dt>USDA Hops Information</dt>
+		<dd> <?php echo get_field('usda_hops_info');?></dd>
 	</dl>
 <?php endif; ?>
-
-<?php if(get_field('cohumulone')){ ?>
-<dl>
-	<dt>Cohumulone (&#37; of alpha acids)</dt>
-	<dd> <?php echo get_field('cohumulone');?>%</dd>
-</dl>
-<?php
-}
-if(get_field('total_oils')){ ?>
-<dl>
-	<dt>Total Oils (Mls. per 100 grams dried hops)</dt>
-	<dd><?php echo get_field('total_oils');?>%</dd></dl>
-	<?php
-}
-if(get_field('myrcene')){ ?>
-<dl>
-	<dt>Myrcene (as % of total oils)</dt>
-	<dd> <?php echo get_field('myrcene');?>%</dd>
-</dl>
-<?php
-}
-if(get_field('caryophyllene')){ ?>
-<dl>
-	<dt>Caryophyllene (as % of total oils)</dt>
-	<dd> <?php echo get_field('caryophyllene');?>%</dd>
-</dl>
-<?php
-}
-if(get_field('humulene')){ ?>
-<dl>
-	<dt>Humulene (as % of total oils)</dt>
-	<dd> <?php echo get_field('humulene');?>%</dd>
-</dl>
-<?php
-}
-if(get_field('farnesene')){ ?>
-<dl>
-	<dt>Farnesene (as % of total oils)</dt>
-	<dd> <?php echo get_field('farnesene');?>%</dd>
-</dl>
-<?php
-}
-if(get_field('storage')){ ?>
-<dl>
-	<dt>Storage (alpha acids remaining after 6 months storage at 20&deg; C))</dt>
-	<dd><?php echo get_field('storage');?></dd>
-</dl>
-<?php
-}
-if(get_field('possible_substitutions')){ ?>
-<dl>
-	<dt>Possible Substitutions</dt>
-	<dd> <?php echo get_field('possible_substitutions');?></dd>
-</dl>
-<?php
-}
-if(get_field('usda_hops_info')){ ?>
-<dl>
-	<dt>USDA Hops Information</dt>
-	<dd> <?php echo get_field('usda_hops_info');?></dd>
-</dl>
-<?php } ?>
