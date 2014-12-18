bfbd41ba6ae0727788feed6457b387bff6b2bd16
Ben Beekman <ben@benbeekman.com>
2014-12-17 16:11:00 -0800

    analytics code fix

--- header.php
+++ header.php
@@ -28,12 +28,10 @@
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		ga('create', 'UA-57801308-1', 'auto');
+ 		ga('send', 'pageview');
 	</script>
-
-  ga('create', 'UA-57801308-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 </head>
 <body <?=body_class()?>>
 
