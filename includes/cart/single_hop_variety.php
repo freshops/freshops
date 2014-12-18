873991addd4220b0e00fd6ab121bc222a0d8b410
Ben Beekman <ben@benbeekman.com>
2014-12-17 19:07:37 -0800

    Added permalinked post object field- substitutions

--- includes/cart/single_hop_variety.php
+++ includes/cart/single_hop_variety.php
@@ -7,23 +7,23 @@ Single hop variety template for use in WP E-commerce single hop pages
 
 
 
-	<?php if(get_field('flavor')): ?>
+<?php if(get_field('flavor')): ?>
+	
+	<dl>
 		
-		<dl>
+		<dt><?php the_title(); ?> Flavor Perception:</dt>
 		
-			<dt><?php the_title(); ?> Flavor Perception:</dt>
-			
-			<dd> <?php echo get_field('flavor');?></dd>
-			
-		</dl>
+		<dd> <?php echo get_field('flavor');?></dd>
 		
-	<?php endif; ?>
+	</dl>
+	
+<?php endif; ?>
 <!-- Begin two-column percentages section -->
 <div class="percentage-columns">
 	<?php if(get_field('example')): ?>
 		
 		<dl>
-		
+			
 			<dt>Commercial Examples: </dt>
 			
 			<dd><?php echo get_field('example');?></dd>
@@ -36,7 +36,7 @@ Single hop variety template for use in WP E-commerce single hop pages
 
 	<?php if(get_field('alpha-min')): ?> <!-- Show alpha range if both min and max have values, otherwise show min . -->
 		<dl>
-		
+			
 			<dt>Acid Range (Alpha &#37;)</dt>
 			
 			<?php (float) $alphamin = get_field('alpha-min'); ?>
@@ -54,24 +54,24 @@ Single hop variety template for use in WP E-commerce single hop pages
 				
 			<?php else: //otherwise alphamin is the alphavalue, so set it accordingly and unset min and max
 			
-				$alphavalue = $alphamin;
-				
+			$alphavalue = $alphamin;
+			
 				unset ($alphamin);  //set to NULL
 				
 				unset ($alphamax);  //set to NULL
 				
-			endif;
-			
-			?>
-			
-			<!-- alphapct= alphavalue converted from a percentage to an integer  -->
-			<?php $alphapct = ( ( (float) $alphavalue) * 0.01); ?>
-
-			<dd class='meter'>
-				<!-- meter begins here -->
-				<!-- set the low and high values based on values for low-alpha and high-alpha hops. -->
+				endif;
 				
-				<meter
+				?>
+				
+				<!-- alphapct= alphavalue converted from a percentage to an integer  -->
+				<?php $alphapct = ( ( (float) $alphavalue) * 0.01); ?>
+
+				<dd class='meter'>
+					<!-- meter begins here -->
+					<!-- set the low and high values based on values for low-alpha and high-alpha hops. -->
+					
+					<meter
 					min="0.03"
 					max="0.16"
 					low="0.07"
@@ -81,20 +81,20 @@ Single hop variety template for use in WP E-commerce single hop pages
 					<?php  //print the min or only value, then if there's a max set, add an emdash and that.
 					if (isset($alphamin))  : ?><?=$alphamin?><?php else : ?><?=$alphavalue;?><?php endif;
 					if (isset($alphamax)) : ?>–<?=$alphamax?><?php endif; ?>%</meter>
-			</dd><!-- meter ends here -->
-			
-					<!-- Display value starts here -->
+				</dd><!-- meter ends here -->
+				
+				<!-- Display value starts here -->
 			<?php  //print the min or only value, then if there's a max set, add an emdash and the max value.
-				if (isset($alphamin))  : ?><?=$alphamin?><?php else : ?><?=$alphavalue;?><?php endif;
-				if (isset($alphamax)) : ?>–<?=$alphamax?><?php endif; ?>%
-				<!-- End Display value -->
+			if (isset($alphamin))  : ?><?=$alphamin?><?php else : ?><?=$alphavalue;?><?php endif;
+			if (isset($alphamax)) : ?>–<?=$alphamax?><?php endif; ?>%
+			<!-- End Display value -->
 		</dl>
 	<?php endif; ?>
 
-<!-- alpha section ends here -->
+	<!-- alpha section ends here -->
 
 
-<!-- beta section starts here -->
+	<!-- beta section starts here -->
 
 	<?php if(get_field('beta-min')) : ?> <!-- Show beta range if both min and max have values, otherwise show min. -->
 		<dl>
@@ -105,7 +105,7 @@ Single hop variety template for use in WP E-commerce single hop pages
 				
 				<?php if (get_field('beta-max')) : 
 					//hyphen followed by max value (if one exists)
-					?>–<?php echo get_field('beta-max');
+				?>–<?php echo get_field('beta-max');
 				endif; ?>%
 			</dd>
 		</dl>
@@ -115,7 +115,7 @@ Single hop variety template for use in WP E-commerce single hop pages
 	<?php if(get_field('cohumulone')): ?>
 		
 		<dl>
-		
+			
 			<dt>Cohumulone (% of alpha acids)</dt>
 			
 			<dd> <?php echo get_field('cohumulone');?>%</dd>
@@ -132,54 +132,77 @@ Single hop variety template for use in WP E-commerce single hop pages
 		<dl>
 			<dt>Total Oils (Mls. per 100 grams dried hops)</dt>
 			<dd><?php echo get_field('total_oils');?>%</dd></dl>
-	<?php endif; ?>
-	
-	<?php if(get_field('myrcene')): ?>
-	<dl>
-		<dt>Myrcene (as % of total oils)</dt>
-		<dd> <?php echo get_field('myrcene');?>%</dd>
-	</dl>
-	<?php endif; ?>
-	<?php if(get_field('caryophyllene')):?>
-		<dl>
-			<dt>Caryophyllene (as % of total oils)</dt>
-			<dd> <?php echo get_field('caryophyllene');?>%</dd>
-		</dl>
-	<?php
-	endif;
-	if(get_field('humulene')): ?>
-		<dl>
-			<dt>Humulene (as % of total oils)</dt>
-			<dd> <?php echo get_field('humulene');?>%</dd>
-		</dl>
-	<?php endif; ?>
+		<?php endif; ?>
+		
+		<?php if(get_field('myrcene')): ?>
+			<dl>
+				<dt>Myrcene (as % of total oils)</dt>
+				<dd> <?php echo get_field('myrcene');?>%</dd>
+			</dl>
+		<?php endif; ?>
+		<?php if(get_field('caryophyllene')):?>
+			<dl>
+				<dt>Caryophyllene (as % of total oils)</dt>
+				<dd> <?php echo get_field('caryophyllene');?>%</dd>
+			</dl>
+			<?php
+			endif;
+			if(get_field('humulene')): ?>
+			<dl>
+				<dt>Humulene (as % of total oils)</dt>
+				<dd> <?php echo get_field('humulene');?>%</dd>
+			</dl>
+		<?php endif; ?>
 
-	<?php if(get_field('farnesene')): ?>
+		<?php if(get_field('farnesene')): ?>
 
-		<dl>
+			<dl>
 
-			<dt>Farnesene (as % of total oils)</dt>
-			
-			<dd> <?php echo get_field('farnesene');?>%</dd>
-			
-		</dl>
-	<?php endif; ?>
+				<dt>Farnesene (as % of total oils)</dt>
+				
+				<dd> <?php echo get_field('farnesene');?>%</dd>
+				
+			</dl>
+		<?php endif; ?>
 
 
 
-<?php if(get_field('storage')): ?>
-	<dl>
-		<dt>Storage (alpha acids remaining after 6 months storage at 20&deg; C))</dt>
-		<dd><?php echo get_field('storage');?></dd>
-	</dl>
-<?php endif;?>
+		<?php if(get_field('storage')): ?>
+			<dl>
+				<dt>Storage (alpha acids remaining after 6 months storage at 20&deg; C))</dt>
+				<dd><?php echo get_field('storage');?></dd>
+			</dl>
+		<?php endif;?>
 
-<?php if(get_field('possible_substitutions')): ?>
-	<dl>
-		<dt>Possible Substitutions</dt>
-		<dd> <?php echo get_field('possible_substitutions');?></dd>
-	</dl>
-<?php endif;?>
+		<?php if(get_field('possible_substitutions')): ?>
+			<dl>
+				<dt>Possible Substitutions</dt>
+				<dd> 
+					<?php
+
+					/*
+					*  Loop through post objects (assuming this is a multi-select field) ( setup postdata )
+					*  Using this method, you can use all the normal WP functions as the $post object is temporarily initialized within the loop
+					*  Read more: http://codex.wordpress.org/Template_Tags/get_posts#Reset_after_Postlists_with_offset
+					*/
+
+					$post_objects = get_field('possible_substitutions');
+
+					if( $post_objects ): ?>
+						<ul>
+							<?php foreach( $post_objects as $post): // variable must be called $post (IMPORTANT) ?>
+								<?php setup_postdata($post); ?>
+								<li>
+									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+					<?php endif;
+
+					?>
+					</dl>
+					<?php endif;?>
 
 </div><!-- end two-column percentage section -->
 
