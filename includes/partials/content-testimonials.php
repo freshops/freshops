058d0c43fbf81e866beb0c13abc8d0d285973150
Micky Hulse <mickyhulse@gmail.com>
2015-01-09 21:35:55 -0800

    Forms fixes.
    See #35.

--- includes/partials/content-testimonials.php
+++ includes/partials/content-testimonials.php
@@ -4,10 +4,14 @@
 */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
-	<section class="entry-content clearfix">
+	<section class="entry-content">
 		<?php the_content(); ?>
 	</section>
+	<?php
+	/*
 	<footer class="article-footer testimonial_attribution"> <!-- The testimonial title serves as the testimonial attribution -->
 		<?php the_title(); ?>
 	</footer>
+	*/
+	?>
 </article>
